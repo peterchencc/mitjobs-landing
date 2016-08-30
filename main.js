580840322bf93a64224c86d9693ece1eb8c01e8d
WangRita <dennyreave@gmail.com>
2016-08-30 19:02:47 +0800

    Avoid scroll effect on carousel control

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 // Smooth scrolling to anchor
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    // Carousel control 
+    if ($(this).attr('class').indexOf("carousel-control") >= 0 ) {
+      
+    }
+    // Scrolling to anchor
+    else {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
       }
     }
   });
